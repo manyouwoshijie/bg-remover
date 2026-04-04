bcab5496f51a3b8ffcc0b96c426031131a1adbd3
manyouwoshijie <Jx720903@gmail.com>
2026-04-04 12:03:34 +0800

    fix: add CloudflareEnv type declaration and fix eslint config paths

---
+++ projects/bg-remover/cloudflare-env.d.ts
@@ -0,0 +1,8 @@
+// Cloudflare env type declarations for next-on-pages
+interface CloudflareEnv {
+  DB: D1Database;
+  GOOGLE_CLIENT_ID: string;
+  GOOGLE_CLIENT_SECRET: string;
+  AUTH_SECRET: string;
+  AUTH_URL: string;
+}
